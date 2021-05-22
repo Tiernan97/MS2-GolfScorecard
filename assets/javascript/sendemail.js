6c8776e8f15ac5193ca5b704d026bed56829ba88
Tiernan97 <tiernanmcdermott.codeinstitute@gmail.com>
2021-05-22 12:53:44 +0000

    About section bootstrap cards added

--- assets/javascript/sendemail.js
+++ assets/javascript/sendemail.js
@@ -1,12 +1,20 @@
+
+(function() {
+            // https://dashboard.emailjs.com/admin/integration
+            emailjs.init("user_1z6S9pOQure77MC4ZJQoz");
+        })();
+
+
+
 window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event) {
                 event.preventDefault();
                 
                 emailjs.sendForm('contact_service', 'contact_form', this)
                     .then(function() {
-                        console.log('SUCCESS!');
+                        console.alert('Your message to Tiernan sent successfully');
                     }, function(error) {
-                        console.log('FAILED...', error);
+                        console.alert('Your message to Tiernan failed to send', error);
                     });
             });
         }  
