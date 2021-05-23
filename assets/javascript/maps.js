6775052f2d618b7eddfe0d919c83e4a6eeb563a9
Tiernan97 <tiernanmcdermott.codeinstitute@gmail.com>
2021-05-23 22:49:56 +0000

    Edits to sendemail.js

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -8,7 +8,7 @@
         });
 
         // Content for on click pop up for markers on Google Map
-        var locations = [
+        var areas = [
         [`<h6 class="popup-header">Frankfurter Golf Club e.V.</h6> <p class="popup-content"> Price: €110 </br> Distance from City Centre <strong>5.3km <i class="fas fa-golf-ball" aria-hidden="true"></i></strong></p>
         <a class="popup-link" href="https://goo.gl/maps/TxpUp2dAHLrnffie9" target="_blank"> Get directions</a>`, 50.07436072207846, 8.634940082778794 ],
         [`<h6 class="popup-header">Golf-Club Hof Hausen vor der Sonne e.V.</h6> <p class="popup-content"> Price: €90 </br> Distance from City Centre <strong>16km <i class="fas fa-golf-ball" aria-hidden="true"></i></strong></p>
@@ -17,7 +17,7 @@
         <a class="popup-link" href="https://goo.gl/maps/5rKNfametEz5HAJT7" target="_blank"> Get directions</a>`, 50.1891297174854, 8.510156025111467 ]
 ];
 
-//Zoom a location by clicking on external button
+//Zoom location
 $(".location-button").click(function() {
   map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
   map.setZoom(15);
@@ -25,12 +25,12 @@ $(".location-button").click(function() {
 
   let infowindow = new google.maps.InfoWindow({});
 
-//Add markers on map according the information in locations array
+//Add markers on the map based on areas array
   let marker, count;
   let bounds = new google.maps.LatLngBounds();
-  for (count = 0; count<locations.length; count++) {
+  for (count = 0; count<areas.length; count++) {
        marker = new google.maps.Marker ({
-       position: new google.maps.LatLng(locations[count][1], locations[count][2]),
+       position: new google.maps.LatLng(areas[count][1], areas[count][2]),
        map: map,
        animation: google.maps.Animation.DROP
   });
@@ -40,7 +40,7 @@ bounds.extend(marker.getPosition());
 // Listener for pop up window content from above line 11-18
 google.maps.event.addListener(marker, 'click', (function (marker, count) {
       return function () {
-        infowindow.setContent(locations[count][0]);
+        infowindow.setContent(areas[count][0]);
         infowindow.open(map, marker);
       }
     })(marker, count));
--- assets/javascript/sendemail.js
+++ assets/javascript/sendemail.js
@@ -1,20 +1,15 @@
-
-(function() {
-            // https://dashboard.emailjs.com/admin/integration
-            emailjs.init("user_1z6S9pOQure77MC4ZJQoz");
-        })();
-
-
-
 window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event) {
                 event.preventDefault();
-                
-                emailjs.sendForm('contact_service', 'contact_form', this)
-                    .then(function() {
-                        console.alert('Your message to Tiernan sent successfully');
-                    }, function(error) {
-                        console.alert('Your message to Tiernan failed to send', error);
-                    });
+                let email=document.getElementById("Email_for_form").value
+
+                console.log(email)
+                // emailjs.sendForm('contact_service', 'contact_form', this)
+                // .then(function() {
+                //         console.alert('Your message to Tiernan sent successfully');
+                //     }, function(error) {
+                //         console.alert('Your message to Tiernan failed to send', error);
+                //     });
             });
         }  
+        
