7393963cca6eaf7e8a902e93a4b7a09beb6ec9d0
Tiernan97 <tiernanmcdermott.codeinstitute@gmail.com>
2021-05-23 23:28:57 +0000

    Success alert % email validation added to form

--- assets/javascript/sendemail.js
+++ assets/javascript/sendemail.js
@@ -1,15 +1,36 @@
+function ValidateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+  {
+    return (true)
+  }
+   
+    return (false)
+}
+
+
 window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event) {
                 event.preventDefault();
                 let email=document.getElementById("Email_for_form").value
 
-                console.log(email)
-                // emailjs.sendForm('contact_service', 'contact_form', this)
-                // .then(function() {
-                //         console.alert('Your message to Tiernan sent successfully');
-                //     }, function(error) {
-                //         console.alert('Your message to Tiernan failed to send', error);
-                //     });
+              if(ValidateEmail(email)){
+                document.getElementById('Email_warning').innerHTML=null
+                  emailjs.sendForm('contact_service', 'contact_form', this)
+                  .then(function() {
+                    document.getElementById('alert-message').innerHTML=` <div class="alert alert-success" role="alert">
+                    Your message was sucessfully sent to Tiernan!
+                  </div>`  
+                  var hidemessage=setTimeout(() => {
+                    document.getElementById('alert-message').innerHTML=null
+                    clearTimeout(hidemessage)
+                  }, 2000); 
+                    console.log('Your message to Tiernan sent successfully');
+                      }, function(error) {
+                          console.log('Your message to Tiernan failed to send', error);
+                      });
+              }else{
+                  document.getElementById('Email_warning').innerHTML='Please enter Correct Email'
+              }
             });
         }  
-        
